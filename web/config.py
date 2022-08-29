cf2142bc43b2bb01f360b030ac4fcf222ffcb4a7
khuchung <khuc.hung@decopac.com>
2022-08-29 18:51:32 +0700

    Update config.py

--- web/config.py
+++ web/config.py
@@ -4,18 +4,18 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL=""  #TODO: Update value
-    POSTGRES_USER="" #TODO: Update value
-    POSTGRES_PW=""   #TODO: Update value
-    POSTGRES_DB=""   #TODO: Update value
+    POSTGRES_URL="udacity3.postgres.database.azure.com"
+    POSTGRES_USER="azureuser@udacity3"
+    POSTGRES_PW="A123456&"
+    POSTGRES_DB="techconfdb"
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://udacity3.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=1mZmdX5R7cTrYsrLVv4A4nLOa91JypeuOms8/cxfGDM='
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
-    ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
-    SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
+    ADMIN_EMAIL_ADDRESS: 'private.hungkm@gmail.com'
+    SENDGRID_API_KEY = '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' #Configuration not required, required SendGrid Account
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
